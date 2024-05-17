6d5c7cf7fc5ff9ca4cee4271970a9bdcaef679a1
Viktor Shpet <shpet.viktor.93@gmail.com>
2024-05-17 23:28:59 +0500

    edit js, add application.yaml

--- src/main/resources/static/JS/list.js
+++ src/main/resources/static/JS/list.js
@@ -1,71 +1,59 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     const audioPlayer = document.getElementById('audioPlayer');
-    const playButtons = document.querySelectorAll('.playButton');
-    const playDiv = document.querySelectorAll('.border-4')
-    let currentIndex = 0;
-    const nextButton = document.getElementById('nextButton');
-    const prevButton = document.getElementById('prevButton');
-    const trackSinger = document.getElementById('trackText');
+    const playButtons = document.querySelectorAll('.playButton')
+    let currentIndex = 0
+    const nextButton = document.getElementById('nextButton')
+    const prevButton = document.getElementById('prevButton')
+    const trackSinger = document.getElementById('trackText')
 
-    nextButton.addEventListener('click', nextTrack);
-    prevButton.addEventListener('click', prevTrack);
+    nextButton.onclick = () => {
+        currentIndex = (currentIndex + 1) % playButtons.length
+        trackSinger.innerText = updateTrackText(currentIndex)
+        loadAudio(currentIndex)
+    }
 
+    prevButton.onclick = () => {
+        currentIndex = (currentIndex - 1 + playButtons.length) % playButtons.length
+        trackSinger.innerText = updateTrackText(currentIndex)
+        loadAudio(currentIndex)
+    }
 
     // Устанавливаем обработчик события клика для всех кнопок Play
-    playButtons.forEach(function (button, index) {
-        button.addEventListener('click', function () {
-            currentIndex = index;
-            loadAudio(currentIndex);
-        });
-    });
-
-    playDiv.forEach(function (div, index) {
-        div.addEventListener('click', function () {
-            currentIndex = index;
-            loadAudio(currentIndex);
-        })
+    playButtons.forEach((button, index) => {
+        button.onclick= () => {
+            currentIndex = index
+            loadAudio(currentIndex)
+        }
     })
 
     // Устанавливаем обработчик события окончания воспроизведения
-    audioPlayer.addEventListener('ended', function () {
-        currentIndex = (currentIndex + 1) % playButtons.length;
-        loadAudio(currentIndex);
-    });
+    audioPlayer.onended = () => {
+        currentIndex = (currentIndex + 1) % playButtons.length
+        loadAudio(currentIndex)
+    }
 
     // Функция для загрузки аудиофайла
     function loadAudio(index) {
         const audioSrc = playButtons[index]?.parentNode?.getAttribute('data-src');
         if (audioSrc) {
-            trackSinger.innerText = updateTrackText(index);
-            audioPlayer.src = audioSrc;
-            audioPlayer.load();
-            audioPlayer.play();
+            trackSinger.innerText = updateTrackText(index)
+            audioPlayer.src = audioSrc
+            audioPlayer.load()
+            audioPlayer.play()
         }
-    }
-
-    function nextTrack() {
-        currentIndex = (currentIndex + 1) % playButtons.length;
-        trackSinger.innerText = updateTrackText(currentIndex);
-        loadAudio(currentIndex);
-    }
-
-    function prevTrack() {
-        currentIndex = (currentIndex - 1 + playButtons.length) % playButtons.length;
-        trackSinger.innerText = updateTrackText(currentIndex);
-        loadAudio(currentIndex);
     }
 
     function updateTrackText(index) {
-        var table = document.getElementById('audioTable');
-        var rows = table.getElementsByTagName('tr');
+        const table = document.getElementById('audioTable')
+        const rows = table.getElementsByTagName('tr')
 
         if (index >= 0 && index < rows.length) {
-            const cells = rows[index + 1].getElementsByTagName('td');
+            const cells = rows[index + 1].getElementsByTagName('td')
 
             if (cells.length >= 3) {
                 // Обновляем значение <span> с новой информацией о треке
-                return cells[0].innerText + ' - ' + cells[1].innerText;
+                return cells[0].innerText + ' - ' + cells[1].innerText
             }
         }
     }
-});
+})
